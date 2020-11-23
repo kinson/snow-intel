f2c701fb7dba163c3db19812d90a4976705267af
Samuel Hunter <swilkinsonhunter@gmail.com>
2020-11-22 21:59:39 -0700

    add server and linting

--- get_traffic.ts
+++ get_traffic.ts
@@ -1,10 +1,19 @@
-import Axios from 'axios';
-import * as B from 'bluebird';
-import * as Cron from 'cron';
-import * as fs from 'fs';
+import Axios from "axios";
+import * as B from "bluebird";
+import * as Cron from "cron";
+import * as fs from "fs";
 
-const readFileAsync: (name: string, encoding: string) => Promise<any> = B.promisify(require("fs").readFile);
-const writeFileAsync: (name: string, contents: string, encoding: string) => Promise<any> = B.promisify(require("fs").writeFile)
+const readFileAsync: (
+  name: string,
+  encoding: string
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+) => Promise<any> = B.promisify(require("fs").readFile);
+const writeFileAsync: (
+  name: string,
+  contents: string,
+  encoding: string
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+) => Promise<any> = B.promisify(require("fs").writeFile);
 
 interface Location {
   Latitude: string;
@@ -21,7 +30,7 @@ interface AlertObject {
   Impact: string;
   IsBothDirectionFlg: string;
   LastUpdatedDate: string;
-  Location: Location
+  Location: Location;
   LocationDescription: string;
   RoadId: string;
   RoadName: string;
@@ -41,41 +50,55 @@ interface AlertsResponse {
 }
 
 async function getTrafficData(): Promise<AlertObject[]> {
-  const closuresData = await Axios.get<AlertsResponse>('https://www.cotrip.org/roadConditions/getLaneClosureAlerts.do');
+  const closuresData = await Axios.get<AlertsResponse>(
+    "https://www.cotrip.org/roadConditions/getLaneClosureAlerts.do"
+  );
   return closuresData.data.Alerts.Alert;
 }
 
-const fileName = './roaddata.json';
-const archiveDirectory = './archive';
-
+const fileName = "./roaddata.json";
+const archiveDirectory = "./archive";
 
 function readJSONFile(): Promise<AlertObject[] | null> {
-  return readFileAsync(fileName, 'utf8').then(JSON.parse)
-  .catch(() => {
-    return null;
-  });
+  return readFileAsync(fileName, "utf8")
+    .then(JSON.parse)
+    .catch(() => {
+      return null;
+    });
 }
 
-function writeToJSONFile(updatedClosures: AlertObject[], savedClosures?: AlertObject[]): Promise<any> {
-  return writeFileAsync(fileName, JSON.stringify(updatedClosures), 'utf8')
-    .then(() => {
+function writeToJSONFile(
+  updatedClosures: AlertObject[],
+  savedClosures?: AlertObject[]
+): Promise<any> {
+  return writeFileAsync(fileName, JSON.stringify(updatedClosures), "utf8").then(
+    () => {
       if (savedClosures) {
-        const timeStamp = (new Date()).toISOString();
-        return writeFileAsync(`${archiveDirectory}/${timeStamp}-road-closures.json`, JSON.stringify(updatedClosures), 'utf8');
+        const timeStamp = new Date().toISOString();
+        return writeFileAsync(
+          `${archiveDirectory}/${timeStamp}-road-closures.json`,
+          JSON.stringify(updatedClosures),
+          "utf8"
+        );
       }
-    });
+    }
+  );
 }
 
 function checkTrafficClosures() {
   return B.all([readJSONFile(), getTrafficData()])
     .then(([savedClosures, updatedClosures]) => {
       if (!savedClosures) {
-        console.log(`Generating new ${fileName} file.`)
+        console.log(`Generating new ${fileName} file.`);
         return writeToJSONFile(updatedClosures);
       }
 
-      const savedClosureAlertIds = savedClosures.map((c: AlertObject) => c.AlertId);
-      const updatedClosureAlertIds = updatedClosures.map((c: AlertObject) => c.AlertId);
+      const savedClosureAlertIds = savedClosures.map(
+        (c: AlertObject) => c.AlertId
+      );
+      const updatedClosureAlertIds = updatedClosures.map(
+        (c: AlertObject) => c.AlertId
+      );
 
       const newClosures = updatedClosures.filter((closure: AlertObject) => {
         return !savedClosureAlertIds.includes(closure.AlertId);
@@ -85,61 +108,79 @@ function checkTrafficClosures() {
         return !updatedClosureAlertIds.includes(closure.AlertId);
       });
 
-      const timeStamp = (new Date()).toISOString();
+      const timeStamp = new Date().toISOString();
 
       if (newClosures) {
         newClosures.forEach((c) => {
-          const directionText = c.IsBothDirectionFlg === 'true' ? 'in both directions' : `going ${c.Direction}`;
-          const mileMarkerText = c.EndMileMarker ? `from mile marker ${c.StartMileMarker} to ${c.EndMileMarker}` : `at mile marker ${c.StartMileMarker}`;
-          const severity = c.RoadwayClosureId === '4' ? '(full)' : '(partial)';
-          console.log(`${timeStamp}: New ${severity} closure on ${c.RoadName} ${directionText} ${mileMarkerText}. From CODOT: ${c.Description}`);
-        })
+          const directionText =
+            c.IsBothDirectionFlg === "true"
+              ? "in both directions"
+              : `going ${c.Direction}`;
+          const mileMarkerText = c.EndMileMarker
+            ? `from mile marker ${c.StartMileMarker} to ${c.EndMileMarker}`
+            : `at mile marker ${c.StartMileMarker}`;
+          const severity = c.RoadwayClosureId === "4" ? "(full)" : "(partial)";
+
+          console.log(
+            `${timeStamp}: New ${severity} closure on ${c.RoadName} ${directionText} ${mileMarkerText}. From CODOT: ${c.Description}`
+          );
+        });
       }
 
       if (newOpenings) {
         newOpenings.forEach((c) => {
-          const directionText = c.IsBothDirectionFlg === 'true' ? 'in both directions' : `going ${c.Direction}`;
-          const mileMarkerText = c.EndMileMarker ? `from mile marker ${c.StartMileMarker} to ${c.EndMileMarker}` : `at mile marker ${c.StartMileMarker}`;
-          console.log(`${timeStamp}: Road reopened on ${c.RoadName} ${directionText} ${mileMarkerText}.`);
+          const directionText =
+            c.IsBothDirectionFlg === "true"
+              ? "in both directions"
+              : `going ${c.Direction}`;
+          const mileMarkerText = c.EndMileMarker
+            ? `from mile marker ${c.StartMileMarker} to ${c.EndMileMarker}`
+            : `at mile marker ${c.StartMileMarker}`;
+
+          console.log(
+            `${timeStamp}: Road reopened on ${c.RoadName} ${directionText} ${mileMarkerText}.`
+          );
         });
       }
 
-      const changePresent = (newOpenings && newOpenings.length !== 0) || (newClosures && newClosures.length !== 0);
+      const changePresent =
+        (newOpenings && newOpenings.length !== 0) ||
+        (newClosures && newClosures.length !== 0);
 
       if (!changePresent) {
         console.log(`${timeStamp}: No new closures or openings`);
-        return writeToJSONFile(updatedClosures)
+        return writeToJSONFile(updatedClosures);
       }
 
       return writeToJSONFile(updatedClosures, savedClosures);
-    }).catch((err) => {
-      console.error('Encountered error fetching new data');
-      console.error(err);
     })
+    .catch((err) => {
+      console.error("Encountered error fetching new data");
+      console.error(err);
+    });
 }
 
-
 async function startJob() {
-  console.log('starting job');
+  console.log("starting job");
 
   try {
     await fs.promises.mkdir(archiveDirectory);
-    console.log('created archive directory');
+    console.log("created archive directory");
   } catch (err) {
-    console.log('archive directory already exists');
+    console.log("archive directory already exists");
   }
 
-  const cronPattern = '*/2 * * * *'
+  const cronPattern = "*/2 * * * *";
   const job = new Cron.CronJob(
     cronPattern,
-    function() {
+    function () {
       return checkTrafficClosures();
     },
     null,
     true
   );
 
-  console.log('job started with cron pattern', cronPattern);
+  console.log("job started with cron pattern", cronPattern);
 }
 
 startJob();
---
+++ server.ts
@@ -0,0 +1,28 @@
+import * as Hapi from "@hapi/hapi";
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  server.route({
+    method: "POST",
+    path: "/register",
+    handler: (request, h) => {
+      console.log(request.headers);
+      console.log(request.body);
+      return "Hello World!";
+    },
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
